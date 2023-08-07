700903203522e8b847ada76809526f69d0b4b06b
GlVerdugo <a01688491@tec.mx>
2023-08-07 13:30:25 -0700

    first commit

---
+++ heart_disease_prediction/tests/test_heart_disease_prediction.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `heart_disease_prediction` package."""
+
+
+import unittest
+
+from heart_disease_prediction import heart_disease_prediction
+
+
+class TestHeart_disease_prediction(unittest.TestCase):
+    """Tests for `heart_disease_prediction` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
