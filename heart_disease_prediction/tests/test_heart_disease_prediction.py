31f558c18cdc8ef429a4cd203734d757b934ebc9
GlVerdugo <140129915+GlVerdugo@users.noreply.github.com>
2023-08-14 22:15:17 -0700

    add test

--- heart_disease_prediction/tests/test_heart_disease_prediction.py
+++ heart_disease_prediction/tests/test_heart_disease_prediction.py
@@ -1,21 +1,45 @@
 #!/usr/bin/env python
 
-"""Tests for `heart_disease_prediction` package."""
+"""Tests for `itesm_mlops` package."""
+import os
+import pytest
+import pandas as pd
 
+from sklearn.pipeline import Pipeline
 
-import unittest
+from heart_disease_prediction.load.load_data import DataRetriever
+from heart_disease_prediction.preprocess.preprocess_data import MissingIndicator
 
-from heart_disease_prediction import heart_disease_prediction
+def does_csv_file_exist(file_path):
+    """
+    Check if a CSV file exists at the specified path.
 
+    Parameters:
+        file_path (str): The path to the CSV file.
 
-class TestHeart_disease_prediction(unittest.TestCase):
-    """Tests for `heart_disease_prediction` package."""
+    Returns:
+        bool: True if the file exists, False otherwise.
+    """
+    return os.path.isfile(file_path)
 
-    def setUp(self):
-        """Set up test fixtures, if any."""
+def test_csv_file_existence():
+    """
+    Test case to check if the CSV file exists.
+    """
+    # Provide the path to your CSV file that needs to be tested
+    os.chdir('Users\glverdugo\Documents\Maestria\MLops\Proyecto\heart_disease_prediction\heart_disease_prediction')
+    csv_file_path = "./data/heart_2020_cleaned.csv"
+    
+    DATASETS_DIR = './data/'
+    
 
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
+    # Call the function to check if the CSV file exists
+    file_exists = does_csv_file_exist(csv_file_path)
 
-    def test_000_something(self):
-        """Test something."""
+    # Use Pytest's assert statement to check if the file exists
+    assert file_exists == True, f"The CSV file at '{csv_file_path}' does not exist."
+
+if __name__ == "__main__":
+    # Run the test function using Pytest
+    pytest.main([__file__])
+
